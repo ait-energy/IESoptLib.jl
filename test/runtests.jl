3cb5544cf8cf02017c4aa334ac6c191a1670951b
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-06-01 21:28:35 +0200

    test: update tests for assets outside src folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,16 +14,19 @@ import JET
 
     @testset "IESoptLib.jl (basic)" verbose = true begin
         path_iesoptlib = abspath(dirname(pathof(IESoptLib)))
+        path_assets = abspath(path_iesoptlib, "..", "assets")
 
-        @test relpath(get_path(:addons), path_iesoptlib) == "addons"
-        @test relpath(get_path(:examples), path_iesoptlib) == "examples"
-        @test relpath(get_path(:templates), path_iesoptlib) == "templates"
+        @test relpath(get_path(:addons), path_assets) == "addons"
+        @test relpath(get_path(:examples), path_assets) == "examples"
+        @test relpath(get_path(:templates), path_assets) == "templates"
 
         @test_throws ArgumentError get_path(:other)
 
         n = "01_basic_single_node"
         @test get_filename(:example, n) == joinpath(get_path(:examples), "$(n).iesopt.yaml")
-        # TODO: test addons & templates
+        n = "CHP"
+        @test get_filename(:template, n) == joinpath(get_path(:templates), "$(n).iesopt.template.yaml")
+        # TODO: test addons
 
         @test_throws ArgumentError get_filename(:other, n)
     end
